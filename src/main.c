e5755f540ad4dc4e71234921d3022067711e39d6
IacobucciB <link00222@gmail.com>
2025-05-26 23:38:02 -0300

    Add pthreads integration in main and create mpi_source file

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "pthreads_source.h"
 
 int main() {
     printf("Hello, world!\n");
+
+    int rank = 4; // Example rank, can be set dynamically
+    pthreads_functions(rank);
+
     return 0;
 }
