f34ca602ca5091b09ea11e93b0e78c5408a1681f
IacobucciB <link00222@gmail.com>
2025-05-27 22:53:38 -0300

    modified:   src/pthreads.c

--- src/pthreads.c
+++ src/pthreads.c
@@ -1,5 +1,70 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+#include <sys/time.h>
+
+//
+// Para tiempo de ejecucion
+//
+
+double dwalltime()
+{
+	double sec;
+	struct timeval tv;
+
+	gettimeofday(&tv,NULL);
+	sec = tv.tv_sec + tv.tv_usec/1000000.0;
+	return sec;
+}
+double tIni, tFin, tTotal;
+
+//
+// Constantes para Algoritmo de gravitacion
+//
+#define PI (3.141592653589793)
+#define M_PI (3.14159265358979323846)
+#define G 6.673e-11
+#define ESTRELLA 0
+#define POLVO 1
+#define H2 2 //Hidrogeno molecular
+
+// ===============
+// ===== CPU =====
+// ===============
+
+//
+// Estructuras y variables para Algoritmo de gravitacion
+//
+typedef struct cuerpo cuerpo_t;
+struct cuerpo{
+	float masa;
+	float px;
+	float py;
+	float pz;
+	float vx;
+	float vy;
+	float vz;
+	float r;
+	float g;
+	float b;
+	int cuerpo;
+};
+
+float *fuerza_totalX,*fuerza_totalY, *fuerza_totalZ;
+float toroide_alfa;
+float toroide_theta;
+float toroide_incremento;
+float toroide_lado;
+float toroide_r;
+float toroide_R;
+
+cuerpo_t *cuerpos;
+int delta_tiempo = 1.0f; //Intervalo de tiempo, longitud de un paso
+int pasos;
+int N;
 
 int rank;
 int T;
@@ -10,34 +75,40 @@ void *pfunction(void *arg) {
     pthread_exit(NULL);
 }
 
-void pthreads_functions(int myRank) {
-    rank = myRank;
-    T = 4; // Example number of threads, can be set dynamically
-    pthread_t threads[T];
-    int threads_ids[T];
-    int id;
-
-    for (id = 0; id < T; id++) {
-        threads_ids[id] = id;
-        if (pthread_create(&threads[id], NULL, pfunction, (void *)&threads_ids[id]) != 0) {
-            perror("Failed to create thread");
-            return;
-        }
-    }
-
-    for (id = 0; id < T; id++) {
-        if (pthread_join(threads[id], NULL) != 0) {
-            perror("Failed to join thread");
-            return;
-        }
-    }
-    
-
+void finalizar(void){
+	free(cuerpos);
+	free(fuerza_totalX);
+	free(fuerza_totalY);
+	free(fuerza_totalZ);
 }
 
 int main(int argc, char const *argv[])
 {
-    int myRank = 0; // Example rank, can be set dynamically
-    pthreads_functions(myRank);
-    return 0;
+    if (argc < 4){
+		printf("Ejecutar: %s <nro. de cuerpos> <DT> <pasos>\n",argv[0]);
+		return -1;
+	}
+
+    N = atoi(argv[1]);
+	delta_tiempo = atof(argv[2]);
+	pasos = atoi(argv[3]);
+	
+	cuerpos = (cuerpo_t*)malloc(sizeof(cuerpo_t)*N);
+	fuerza_totalX = (float*)malloc(sizeof(float)*N);
+	fuerza_totalY = (float*)malloc(sizeof(float)*N);
+	fuerza_totalZ = (float*)malloc(sizeof(float)*N);
+
+    tIni = dwalltime(); 
+	
+	int paso;
+	for(paso=0; paso<pasos; paso++){
+		gravitacionCPU(cuerpos,N,delta_tiempo);
+	}
+
+	tFin =	dwalltime();
+	tTotal = tFin - tIni;
+	
+	printf("Tiempo en segundos: %f\n",tTotal);
+	finalizar();
+    return(0);
 }
