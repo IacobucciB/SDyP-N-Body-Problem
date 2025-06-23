5b51bea57f2436177373e2503df156548126a604
IacobucciB <link00222@gmail.com>
2025-06-23 09:52:24 -0300

    added run_mpi

---
+++ src/run_mpi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mpicc -o mpi mpi.c -lm -pthread
+if [ $? -ne 0 ]; then
+    echo "Error: Compilation failed."
+    exit 1
+fi
+
+mpirun --oversubscribe --bind-to none -np 2 mpi 512 200 1000 2 > outpu_mpi.txt
+if [ $? -ne 0 ]; then
+    echo "Error: MPI execution failed."
+    exit 1
+fi
+
+echo "Execution completed successfully."
