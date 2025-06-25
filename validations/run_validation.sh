ad5822e089b637c5323133eaae844a2c33b54cc4
IacobucciB <link00222@gmail.com>
2025-06-25 19:06:31 -0300

    validaciones faltantes

--- validations/run_validation.sh
+++ validations/run_validation.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-# Run all other run_validation.sh scripts in Pthreads subfolders
+# Limpiar archivos de salida previos
+> all_validations_pthreads.txt
+> all_validations_mpi.txt
+
+# Ejecutar todos los run_validation.sh en Pthreads
 for core in 4-cores 8-cores; do
-  for size in 1024 512; do
+  for size in 512 1024 2048 4096; do
     script="Pthreads/$core/$size/run_validation.sh"
     if [ -f "$script" ]; then
-      echo "\n==== Running $script ====" >> all_validations_pthreads.txt
+      echo -e "\n==== Running $script ====" >> all_validations_pthreads.txt
       bash "$script" >> all_validations_pthreads.txt
     fi
   done
 done
 
-# Run all other run_validation.sh scripts in openMPI-Pthreads subfolders
+# Ejecutar todos los run_validation.sh en openMPI-Pthreads
 for core in 4-cores 8-cores 16-cores; do
-  for size in 1024 2048 4096 512; do
+  for size in 512 1024 2048 4096; do
     script="openMPI-Pthreads/$core/$size/run_validation.sh"
     if [ -f "$script" ]; then
-      echo "\n==== Running $script ====" >> all_validations_mpi.txt
+      echo -e "\n==== Running $script ====" >> all_validations_mpi.txt
       bash "$script" >> all_validations_mpi.txt
     fi
   done
